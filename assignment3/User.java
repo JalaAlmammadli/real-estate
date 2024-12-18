bda9e3d3dc1c229e5f829aae9c6c3cd63ebe4fad
Ayan Jamal <ajamal16112@ada.edu.az>
2024-12-18 14:26:01 +0300

    Updated User.java, new gets and sets for seller

--- assignment3/User.java
+++ assignment3/User.java
@@ -1,16 +1,24 @@
 package assignment3;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
     private String username;
     private String password;
     private String role;
     private String email;
+    private int messageCount;
+    private Set<String> messageSenders;
 
-    public User(String username, String password, String role) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.email = username + "@email.com";
-    }
+public User(String username, String password, String role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.email = username + "@email.com";
+    this.messageCount = 0;
+    this.messageSenders = new HashSet<>();
+}
 
     public String getUsername() {
         return username;
@@ -32,4 +40,17 @@ public class User {
     public String toString() {
         return username + "," + password + "," + role + "," + email;
     }
+    public void incrementMessageCount(String sender) {
+        messageCount++;
+        messageSenders.add(sender);
+    }
+    
+    public int getMessageCount() {
+        return messageCount;
+    }
+    
+    public Set<String> getMessageSenders() {
+        return messageSenders;
+    }
+    
 }
